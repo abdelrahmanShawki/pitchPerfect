a8fc274b963641607901449bc6e864cf040e4df7
shawki <mac@MACs-MacBook>
2021-01-31 13:41:15 +0200

    lesson 3 in iOS first project , -application life cycle 1 - not running 2 - in active 3 - active 4 - background 5 - suspended - open apple documentation and find what i search for - did vs. will in ant function i use - enabling and disabling button - multiple views - UI navigator control and add new UIview from tool box

--- pitchperfect/ViewController.swift
+++ pitchperfect/ViewController.swift
@@ -9,14 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordLabel: UILabel!
+    
+    @IBOutlet weak var recordAudioOutlet: UIButton!
+    @IBOutlet weak var stopOutlet: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        ////v1
-        //v2
+        stopOutlet.isEnabled = false
+       
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
     }
         
     
-
+    @IBAction func recordAudio(_ sender: Any) {
+        stopOutlet.isEnabled = true
+        recordAudioOutlet.isEnabled = false
+        recordLabel.text = "reacrding in progress"
+    }
+    
+    @IBAction func stopRecord(_ sender: Any) {
+        recordAudioOutlet.isEnabled = true
+        stopOutlet.isEnabled = false
+        recordLabel.text = "tab to record"
+    }
 }
 
