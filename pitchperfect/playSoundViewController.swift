c2974d3a8e67d134e85a00e29743d138a91dff3a
shawki <mac@MACs-MacBook>
2021-02-07 07:39:10 +0200

    7 feb  > play sound view controller have a positioning problem

---
+++ pitchperfect/playSoundViewController.swift
@@ -0,0 +1,18 @@
+//
+//  playSoundViewController.swift
+//  pitchperfect
+//
+//  Created by shawki on 05/02/2021.
+//
+
+import UIKit
+
+class playSoundViewController: UIViewController {
+    var recordedAudioURL : URL!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    
+
+    }
+}
