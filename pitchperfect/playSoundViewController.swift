f55a769664d78699bd026fe6ebbaafd3d03ac481
shawki <shawki>
2021-02-20 05:17:31 +0200

    error in extension file

--- pitchperfect/playSoundViewController.swift
+++ pitchperfect/playSoundViewController.swift
@@ -6,13 +6,45 @@
 //
 
 import UIKit
-
+import AVFoundation
 class playSoundViewController: UIViewController {
+    @IBOutlet weak var snailButton: UIButton!
+    @IBOutlet weak var chipmunkButton: UIButton!
+    @IBOutlet weak var rabbitButton: UIButton!
+    @IBOutlet weak var vaderButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
     var recordedAudioURL : URL!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
     
+    
+    var audioFile:AVAudioFile!
+    var audioEngine:AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: Timer!
 
+    enum ButtonType: Int {
+        case slow = 0, fast, chipmunk, vader, echo, reverb
+    }
+    
+    
+    
+    
+    @IBAction func playSoundForButton(_ sender: UIButton) {
+        print("Play Sound Button Pressed")
+    }
+
+    @IBAction func stopButtonPressed(_ sender: AnyObject) {
+        print("Stop Audio Button Pressed")
+    }
+    override func viewDidLoad() {
+       
+        super.viewDidLoad()
+        setupAudio()
+    
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        configureUI(.notPlaying)
     }
 }
